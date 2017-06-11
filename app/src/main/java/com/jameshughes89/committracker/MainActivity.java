44de5e83f9b61b0cbf8a76cc3c9da6280763b82a
James Hughes <JamesHughes89@Gmail.com>
2017-06-11 13:12:25 -0400

    Rip off the fratment exampe from android for the GUI and functionality.
    I started making the Tracker object. I still need to finish the updateTime (both the delta, and the possibility of another exception if matcher.find() returns false).
    
    TODO:
    - Build the structure
    - Copy the Date/Time functionality over from the java project
    - Say who did it
    - Put committer picture
    - Put the description of the commit there
    - Say which files changed (maybeee)
    - Push notifications (with settings)
    - Widget

--- app/src/main/java/com/jameshughes89/committracker/MainActivity.java
+++ app/src/main/java/com/jameshughes89/committracker/MainActivity.java
@@ -3,6 +3,21 @@ package com.jameshughes89.committracker;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * Created by JamesHughes89 on 6/09/2017.
+ *
+ * A small app that will keep you informed about when your repos, or repos you're interested in
+ * have updates. It will also say who updated them, and the commit message.
+ *
+ * WARNING: This uses HTML tags for parsing. This is probably a bad idea long term because
+ *          they could change what their tags are.
+ *
+ * Credits:
+ *  Creator - James Alexander Hughes
+ *      Art - Matea Drljepan
+ *
+ */
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
---
+++ app/src/main/java/com/jameshughes89/committracker/Tracker.java
@@ -0,0 +1,160 @@
+package com.jameshughes89.committracker;
+
+import android.graphics.Picture;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by JamesHughes89 on 6/11/2017.
+ *
+ * This class will keep track of a single repository's details.
+ * These details are saved and will be presented in the list,
+ * and/or the project details view (fragment?).
+ */
+
+public class Tracker {
+
+    // REGEX: The tags for the different details from the HTML text
+    // THIS IS WHAT WILL NEED TO BE UPDATED IF THE HTML TAGS EVER CHANGE ON GITHUB
+    private final String TIME_TAG_REGEX = "<relative-time datetime=\"(.+?)\">";
+
+    // These should stay the same throughout the lifetime of the Tracker
+    private String projectUser;
+    private String projectName;
+    private URL projectURL;
+
+    // These will update over the life of the Tracker
+    private String commitTime;
+    private String commitTimeDelta;
+    private String commitDescription;
+    private String commitUser;
+    private Picture commitUserPicture;
+
+    /**
+     * Default constructor. Basically useless. Should never get called.
+     */
+    public Tracker() throws MalformedURLException{
+        this("jameshughes89", "committracker");
+    }
+
+    /**
+     * Constructor that should basically always be used. Will create the Tracker and assign the
+     * project user, name, and URL
+     *
+     * @param projectUser The user/orginisation that created the repository.
+     * @param projectName The name of the repository.
+     */
+    public Tracker(String projectUser, String projectName) throws MalformedURLException{
+        this.projectUser = projectUser;
+        this.projectName = projectName;
+        projectURL = new URL("https://github.com/" + projectUser + "/" + projectName);
+    }
+
+    /**
+     * Updates the details for the tracker. This includes:
+     * - commitTime
+     * - commitDescription
+     * - commitUser
+     * - commitUserPicture
+     *
+     * Will read the URL details and put it into one long string.
+     */
+    public void update(){
+        try{
+            String HTMLtext = getHTMLtext();
+            updateTimeAndTimeDelta(HTMLtext);
+        }
+        catch (IOException | ParseException e){
+            // Currently do nothing? This may make sense... Just leave things alone. Maybe a popup?
+        }
+
+    }
+
+    /**
+     * Will return a String version of the HTML text from the project's URL. Uses a StringBuilder
+     * because that's the right thing to do here.
+     *
+     * @return The String version of the HTML text from the project URL.
+     * @throws IOException
+     */
+    private String getHTMLtext() throws IOException{
+        BufferedReader in = new BufferedReader(new InputStreamReader(projectURL.openStream()));
+        StringBuilder sb = new StringBuilder();
+        String inputLine;
+        while ((inputLine = in.readLine()) != null)
+            sb.append(inputLine);
+        in.close();
+        return sb.toString();
+    }
+
+
+    /**
+     * Updates both the commitTime and TimeDelta (time since commit).
+     *
+     * @param HTMLtext HTML text from the project's website.
+     * @throws ParseException
+     */
+    private void updateTimeAndTimeDelta(String HTMLtext) throws ParseException{
+
+        // Finds the string for the date
+        final Pattern pattern = Pattern.compile(TIME_TAG_REGEX);
+        final Matcher matcher = pattern.matcher(HTMLtext);
+
+        // TODO
+        // Consider throwing an exception here? This statement returns a boolena. If false, it wasn't found, therefore maybe do nothing (throw exception)?
+        matcher.find();
+
+        System.out.println(matcher.group(1)); // Prints String I want to extract
+        String theDateTime = matcher.group(1);
+
+        // Get a nice, more *normal* version of the commit time. This will also be local time zone too.
+        DateFormat m_ISO8601Local = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+        commitTime = m_ISO8601Local.parse(theDateTime).toString();
+
+        // TODO
+        // Gets the delta time.
+
+    }
+
+
+
+    // A bunch of getters follow. Some may not be needed
+
+    public String getProjectUser() {
+        return projectUser;
+    }
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public URL getProjectURL() {
+        return projectURL;
+    }
+
+    public String getCommitTime() {
+        return commitTime;
+    }
+
+    public String getCommitDescription() {
+        return commitDescription;
+    }
+
+    public String getCommitUser() {
+        return commitUser;
+    }
+
+    public Picture getCommitUserPicture() {
+        return commitUserPicture;
+    }
+
+}
